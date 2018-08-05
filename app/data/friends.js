24f3249ba382ea87933769ccf1a4a443535f9234
Kevin Kafuku <kevkafuku@gmail.com>
2018-08-04 23:44:35 -0400

    logic worked out, doing some more styling and heroku deployment soon

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+    {
+        name: "Ross Geller",
+        pic: "https://img.maximummedia.ie/her_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtaGVyLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE3XFxcLzA1XFxcLzAzMTI1MjI4XFxcL3Jvc3MtMTAyNHg1NzAuanBnXCIsXCJ3aWR0aFwiOjc2NyxcImhlaWdodFwiOjQzMSxcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3Lmhlci5pZVxcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2hlclxcXC9uby1pbWFnZS5wbmc_dj01XCJ9IiwiaGFzaCI6IjIzYjI0ZDVlYWM3OWMyYTY3MzZjMmMxNTA5MTEzODhkY2U0NjIzZGEifQ==/ross-1024x570.jpg",
+        scores: [
+            "1",
+            "3", 
+            "4", 
+            "3", 
+            "5", 
+            "4", 
+            "4", 
+            "2",
+            "2", 
+            "1"
+        ]
+    },
+    {
+        name: "Monica Geller",
+        pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP9P0R_mATZ0lV5zf4jfa-ptEon3QoA64CVjPS90INUWCGDhST",
+        scores: [
+            "5",
+            "1", 
+            "1", 
+            "1", 
+            "1", 
+            "5", 
+            "3", 
+            "4",
+            "3", 
+            "1"
+        ]
+    },
+    {
+        name: "Chandler Bing",
+        pic: "https://cdn.revistagq.com/uploads/images/thumbs/es/gq/4/s/2016/26/moda_en_los_90_chandler_bing_friends_812376130_1500x844.jpg",
+        scores: [
+            "5",
+            "1", 
+            "1", 
+            "1", 
+            "1", 
+            "5", 
+            "5", 
+            "2",
+            "1", 
+            "4"
+        ]
+    },
+    {
+        name: "Joey Tribbiani",
+        pic: "https://media2.mensxp.com/media/content/2015/Jan/timesjoeytribbianiprovedthathewasactuallythewisestinthegrouph_1421748100_1100x513.jpg",
+        scores: [
+            "5",
+            "5", 
+            "5", 
+            "5", 
+            "1", 
+            "5", 
+            "1", 
+            "1",
+            "1", 
+            "1"
+        ]
+    },
+    {
+        name: "Rachel Green",
+        pic: "https://thoughtcatalog.files.wordpress.com/2017/01/rachel_green_4.jpg?w=1050&h=751",
+        scores: [
+            "5",
+            "5", 
+            "5", 
+            "5", 
+            "1", 
+            "5", 
+            "1", 
+            "1",
+            "1", 
+            "1"
+        ]
+    },
+    {
+        name: "Phoebe Buffay",
+        pic: "https://i.pinimg.com/originals/8f/63/12/8f631249393db12cc105bd62ef017315.jpg",
+        scores: [
+            "5",
+            "5", 
+            "5", 
+            "5", 
+            "1", 
+            "5", 
+            "1", 
+            "1",
+            "1", 
+            "1"
+        ]
+    },
+    ];
+    
+    module.exports = friends;
